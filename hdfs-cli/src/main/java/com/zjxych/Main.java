ecc9d123fed382c107dd73baf395b70f1ead1dc2
dell <503308294@qq.com>
2021-08-10 11:14:18 +0800

    8-10

--- hdfs-cli/src/main/java/com/zjxych/Main.java
+++ hdfs-cli/src/main/java/com/zjxych/Main.java
@@ -1,6 +1,7 @@
 package com.zjxych;
 
 import java.io.*;
+import java.util.Arrays;
 
 import org.gdal.gdal.Band;
 import org.gdal.gdal.Dataset;
@@ -25,15 +26,36 @@ public class Main {
         //readTiff
         gdal.AllRegister();
 
-        String rasterFilePath = "\\\\192.168.2.2\\GISData\\1800_dlgh\\zj_dem.tif";
+//        String rasterFilePath = "\\\\192.168.2.2\\GISData\\1800_dlgh\\zj_dem.tif";
+        String rasterFilePath = "D:\\romote sense Pro\\zj_dem.tif";
 
-        Dataset dataset;
-        dataset = gdal.Open(rasterFilePath,
-                gdalconstConstants.GA_ReadOnly);
+        Dataset dataset = gdal.Open(rasterFilePath);
         if (dataset == null) {
             System.out.println("GDAL read error: " + gdal.GetLastErrorMsg());
         }
+        Driver driver = dataset.GetDriver();
+        System.out.println("Driver: " + driver.getShortName() + "/" + driver.getLongName());
 
+        // 读取影像信息
+        int xSize = dataset.getRasterXSize();
+        int ySzie = dataset.getRasterYSize();
+        int nBandCount = dataset.getRasterCount();
+        System.out.println("xSize is " + xSize + ",\nySize is" + ySzie + ",\nbandscount is " + nBandCount);
+        int layercount = dataset.GetLayerCount();
+        System.out.println("layercount is "+layercount);
+        Band band = dataset.GetRasterBand(1);
+        int type = band.GetRasterDataType();
+        System.out.println("type is "+type);
+        double[] transform = dataset.GetGeoTransform();
+        System.out.println("top left x:"+transform[0]);
+        System.out.println("w--e pixel resolution:"+transform[1]);
+        System.out.println("rotation:"+transform[2]);
+        System.out.println("top left y:"+transform[3]);
+        System.out.println("rotation:"+transform[4]);
+        System.out.println("n--s pixel resolution:"+transform[5]);
+        dataset.delete();
+
+        gdal.GDALDestroyDriverManager();
         // client.writeRasterPixel("/testRR", 0, 0, 0, 0, 23);
         // client.writeRasterPixel("/testRR", 0, 0, 1, 0, 65);
         // client.writeRasterPixel("/testRR", 0, 0, 2, 0, 83);
@@ -44,6 +66,5 @@ public class Main {
 //        for (LocatedFileStatus fileStatus : client.ListFiles("/")) {
 //            System.out.println(fileStatus.getPath());
 //        }
-        System.out.println();
     }
 }
